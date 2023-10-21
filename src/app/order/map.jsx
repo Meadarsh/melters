85cd4f4a33bb162f0ac7f1f45e9df46c439b7711
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2023-10-21 17:59:28 +0530

    Update map.jsx

--- src/app/order/map.jsx
+++ src/app/order/map.jsx
@@ -1,14 +1,13 @@
 "use client";
 import React, { useEffect, useState ,useRef} from "react";
-import "leaflet/dist/leaflet.css";
+
 import "./style.css";
-//import { useGeolocation } from "./Location";
-import { MapContainer, TileLayer, Circle, } from "react-leaflet";
+
 const Map = () => {
  // const { isAvailable, isEnabled, coordinates } = useGeolocation();
-  const mapRef = useRef(null);
  
- const[clientLocation,setClientLocation]=useState([20,80,4]);
+ 
+
  /*
  useEffect(() => {
   const updateClientLocation = () => {
@@ -28,10 +27,7 @@ const Map = () => {
  
   */ 
   
-  if (mapRef.current) {
-    mapRef.current.flyTo([clientLocation[0],clientLocation[1]], clientLocation[2]);
-  }
-  const zoom=4.5;
+  
   return (
     <>
      {/*isAvailable ? (
@@ -55,16 +51,7 @@ const Map = () => {
         <p>Your browser does not support geolocation.</p>
       )*/}
    <div className="map-container">
-          <MapContainer center={[20, 80]} zoom={zoom} ref={mapRef}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <TileLayer
-              url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
-              attribution='&copy;  <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>"> contributors'
-            />
-          </MapContainer>
+          <h1>Hello</h1>
         </div>
     </>
   );
