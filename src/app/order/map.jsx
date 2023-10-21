1730459446c7362ffb7d620aeb94c57f10bd1369
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2023-10-21 18:11:04 +0530

    Update map.jsx

--- src/app/order/map.jsx
+++ src/app/order/map.jsx
@@ -37,6 +37,16 @@ function LocationComponent() {
       ) : (
         <p>Getting location...</p>
       )}
+       <MapContainer center={[20, 80]} zoom={10} >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <TileLayer
+              url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+              attribution='&copy;  <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a>"> contributors'
+            />
+          </MapContainer>
     </div>
   );
 }
