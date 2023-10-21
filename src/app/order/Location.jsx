e28c5daaef0cfb50365cdd08d5d12f6862745d0a
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2023-10-21 17:47:25 +0530

    Update Location.jsx

--- src/app/order/Location.jsx
+++ src/app/order/Location.jsx
@@ -4,12 +4,18 @@ import { useGeolocated } from "react-geolocated";
 export function useGeolocation() {
   const { isGeolocationAvailable, isGeolocationEnabled, coords } = useGeolocated();
 
-
+  if (typeof window !== 'undefined') {
     return {
       isAvailable: isGeolocationAvailable,
       isEnabled: isGeolocationEnabled,
       coordinates: coords,
     };
- 
+  } else {
+    return {
+      isAvailable: false,
+      isEnabled: false,
+      coordinates: null,
+    };
+  }
 }
 
